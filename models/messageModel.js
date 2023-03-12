6cefa4a4d0ea6110a198755b3d3572c466680c63
Anbarasan R <anbarasan20001@gmail.com>
2023-03-12 16:17:38 +0530

    message model setup

---
+++ models/messageModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("messages", messageSchema);
